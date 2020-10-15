b9afbac22f7b9d7b172ad93630affb34729477a8
adamblo <38425775+adamblo@users.noreply.github.com>
2020-10-15 19:57:07 +0200

    TDD attacking of monsters

--- InteProjektarbete/src/main/java/se/su/dsv/projektarbete/unit/Monster.java
+++ InteProjektarbete/src/main/java/se/su/dsv/projektarbete/unit/Monster.java
@@ -2,13 +2,23 @@ package main.java.se.su.dsv.projektarbete.unit;
 
 public class Monster {
     private String name;
+    private boolean isGrounded;
 
     public Monster(String name){
         setName(name);
+        if(name.equals("Bat")){ //NEED TO SUBCLASS
+            isGrounded = false;
+        }else{
+            isGrounded = true;
+        }
     }
 
     public String getName() {
         return name;
+    }
+
+    public boolean isGrounded(){
+        return isGrounded;
     }
 
     private void setName(String name){
@@ -19,6 +29,9 @@ public class Monster {
     }
 
     public boolean Attack(Monster enemy){
+        if(isGrounded() && !enemy.isGrounded()){
+            return false;
+        }
         return true;
     }
 }
