a5aa5737c97b5d1d080d3220e83555da4807fcdf
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 14:09:57 +0200

    Basic getDurability method that gives test case a success.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -5,4 +5,7 @@ public class BreastplateOfTesting extends Chest{
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", "An excellent breastplate for testing things with!", 150, 0, 2, 5, 3);
 	}
+    public int getDurability() {
+    	return 100;
+    }
 }
