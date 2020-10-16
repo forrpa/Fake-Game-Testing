6b32ce78c5ef558ba596ffd0fc5c46d35cd23441
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 12:03:34 +0200

    tillagd metod

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -3,4 +3,7 @@ package equipment;
 public class BreastplateOfTesting{
     private String name = "Breastplate of Testing)";
     public String getName(){return this.name;}
+    public int[] getAttributes() {
+    	return new int[]{0,2,5,3};
+    }
 }
