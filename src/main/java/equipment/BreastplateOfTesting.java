f71988bbd91c35d6f735f27f340cdd90f1731f1e
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 14:11:04 +0200

    refactored durability in breastplate class.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -1,11 +1,12 @@
 package equipment;
 
 public class BreastplateOfTesting extends Chest{
+	private int durability = 100;
 	
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", "An excellent breastplate for testing things with!", 150, 0, 2, 5, 3);
 	}
     public int getDurability() {
-    	return 100;
+    	return durability;
     }
 }
