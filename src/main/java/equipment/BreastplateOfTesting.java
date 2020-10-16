1627794b860ed9b26272f03663c42433decd908b
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 15:21:25 +0200

    test case succeeds, but previous test case fails. 2 methods with 2 different intended functions have the same name.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -5,4 +5,8 @@ public class BreastplateOfTesting extends Chest{
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", "An excellent breastplate for testing things with!", 150, 100, 0, 2, 5, 3);
 	}
+    public int getDurability() {
+		return 87;
+    	
+    }
 }
