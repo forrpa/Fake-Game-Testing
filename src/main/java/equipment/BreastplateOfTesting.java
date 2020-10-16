6285eb5192caa853191abc38fcea40a40aef6862
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 13:01:44 +0200

    attributes uppflyttade till Equipment

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -3,14 +3,6 @@ package equipment;
 public class BreastplateOfTesting extends Chest{
 	
     public BreastplateOfTesting() {
-		super("Breastplate of Testing");
+		super("Breastplate of Testing", 0, 2, 5, 3);
 	}
-    private int strength = 0;
-    private int agility = 2;
-    private int intelligence = 5;
-    private int stamina = 3;
-    
-    public int[] getAttributes() {
-    	return new int[]{this.strength, this.agility, this.intelligence, this.stamina};
-    }
 }
