4cf5c1e4958ecae8c8bfa534db86be7e7b14f37d
Jennifer <jemc7787@student.su.se>
2020-10-16 13:40:53 +0200

    Fixed so test playerMeetsEndRequirementsForTalkToGuildLeaderQuest now works

--- src/test/java/quest/QuestTest.java
+++ src/test/java/quest/QuestTest.java
@@ -29,8 +29,8 @@ class QuestTest {
     void playerMeetsEndRequirementsForTalkToGuildLeaderQuest(){
         //Slutkrav: Ha pratat med guildledaren + ha Guild Map i inventory
         Player player = new Player("Healer", "Orc", 200, 200, 1500);
-        TalkToGuildLeader quest = new TalkToGuildLeader("TTGL", "Talk", true, true);
-        assertTrue(quest.talkedToGuildLeader);
+        TalkToGuildLeader quest = new TalkToGuildLeader("TTGL", "Talk", "in progress", true, true);
+        assertTrue(quest.isTalkedToGuildLeader());
         assertTrue(player.getInventory().contains("Guild Map"));
     }
 }
