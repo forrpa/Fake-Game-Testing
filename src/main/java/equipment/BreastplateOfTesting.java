bd7bceb0b09276e50c1c149c589bdba8d92dc464
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 12:42:35 +0200

    Flyttat namn-metoder och klassvariabel till superklassen.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -1,13 +1,15 @@
 package equipment;
 
 public class BreastplateOfTesting extends Chest{
-    private String name = "Breastplate of Testing";
+	
+    public BreastplateOfTesting() {
+		super("Breastplate of Testing");
+	}
     private int strength = 0;
     private int agility = 2;
     private int intelligence = 5;
     private int stamina = 3;
     
-    public String getName(){return this.name;}
     public int[] getAttributes() {
     	return new int[]{this.strength, this.agility, this.intelligence, this.stamina};
     }
