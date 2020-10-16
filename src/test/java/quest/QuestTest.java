6cf980a7311d90fc9d5c6d1ecf771fede1fee7a4
Jennifer <jemc7787@student.su.se>
2020-10-16 13:19:00 +0200

    Added features to TalkToGuildLeader so that the test playerMeetsStartRequirementsForTalkToGuildLeaderQuest makes sense. New test for end requirements that currently is not working.

--- src/test/java/quest/QuestTest.java
+++ src/test/java/quest/QuestTest.java
@@ -24,4 +24,13 @@ class QuestTest {
         //assertTrue(player.getInventory().contains(quest.getQuestItem));
         assertTrue(player.getInventory().contains("Guild Map"));
     }
+
+    @Test
+    void playerMeetsEndRequirementsForTalkToGuildLeaderQuest(){
+        //Slutkrav: Ha pratat med guildledaren + ha Guild Map i inventory
+        Player player = new Player("Healer", "Orc", 200, 200, 1500);
+        TalkToGuildLeader quest = new TalkToGuildLeader("TTGL", "Talk", true, true);
+        assertTrue(quest.talkedToGuildLeader);
+        assertTrue(player.getInventory().contains("Guild Map"));
+    }
 }
