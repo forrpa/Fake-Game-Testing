adf55dcf3a47b673122bf4376198ebbcc5561d95
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 13:09:42 +0200

    metoden getDescription förfinad

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -1,11 +1,12 @@
 package equipment;
 
 public class BreastplateOfTesting extends Chest{
+	private String description = "An excellent breastplate for testing things with!";
 	
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", 0, 2, 5, 3);
 	}
     public String getDescription() {
-    	return "An excellent breastplate for testing things with!";
+    	return this.description;
     }
 }
