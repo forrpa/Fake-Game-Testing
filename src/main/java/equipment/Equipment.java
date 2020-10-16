644bd14571b3c03828aa021e5fee74bda24251b8
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 13:12:03 +0200

    description uppflyttad till Equipment superklass.

--- src/main/java/equipment/Equipment.java
+++ src/main/java/equipment/Equipment.java
@@ -6,16 +6,21 @@ public class Equipment{
     private int agility;
     private int intelligence;
     private int stamina;
+    private String description;
     
-	public Equipment(String name, int str, int agi, int intell, int sta) {
+	public Equipment(String name, String desc, int str, int agi, int intell, int sta) {
 		this.name = name;
 		this.strength = str;
 		this.agility = agi;
 		this.intelligence = intell;
 		this.stamina = sta;
+		this.description = desc;
 	}
     public String getName(){return this.name;}
     public int[] getAttributes() {
     	return new int[]{this.strength, this.agility, this.intelligence, this.stamina};
+    }
+    public String getDescription() {
+    	return this.description;
     }
 }
