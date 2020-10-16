70f34586df222e40b63ee39ed16dc3a3bf29e42c
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 13:49:06 +0200

    armor in breastplate class slightly better.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -1,11 +1,12 @@
 package equipment;
 
 public class BreastplateOfTesting extends Chest{
+	private int armor = 150;
 	
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", "An excellent breastplate for testing things with!", 0, 2, 5, 3);
 	}
     public int getArmor() {
-    	return 150;
+    	return this.armor;
     }
 }
