f2ec94c279d0c27cd36395884d353ecc104f71a1
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 15:24:36 +0200

    improved durability method slightly.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -1,12 +1,13 @@
 package equipment;
 
 public class BreastplateOfTesting extends Chest{
+	private int durability = 87;
 	
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", "An excellent breastplate for testing things with!", 150, 100, 0, 2, 5, 3);
 	}
     public int getDurability() {
-		return 87;
+		return this.durability;
     	
     }
 }
