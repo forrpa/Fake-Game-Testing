c9177dec24dadda1a7a67e43fac3c938fd37f7b0
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 13:08:21 +0200

    testcase hasCorrectDescription kommer nu ge rätt

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -5,4 +5,7 @@ public class BreastplateOfTesting extends Chest{
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", 0, 2, 5, 3);
 	}
+    public String getDescription() {
+    	return "An excellent breastplate for testing things with!";
+    }
 }
