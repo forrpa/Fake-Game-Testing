28a43b3a38cdd8cb1ae590b62138ea7ec2d82dc8
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 12:21:23 +0200

    refactoring getAttributes

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -2,8 +2,13 @@ package equipment;
 
 public class BreastplateOfTesting{
     private String name = "Breastplate of Testing";
+    private int strength = 0;
+    private int agility = 2;
+    private int intelligence = 5;
+    private int stamina = 3;
+    
     public String getName(){return this.name;}
     public int[] getAttributes() {
-    	return new int[]{0,2,5,3};
+    	return new int[]{this.strength, this.agility, this.intelligence, this.stamina};
     }
 }
