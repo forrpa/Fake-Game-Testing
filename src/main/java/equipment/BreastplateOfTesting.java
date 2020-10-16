4f92c5a2e02b048cb6852598f1ef198d5abfc641
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 12:38:17 +0200

    CHest tillagds som superclass till Breastplate of Testing

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -1,18 +1,14 @@
 package equipment;
 
-public class BreastplateOfTesting{
+public class BreastplateOfTesting extends Chest{
     private String name = "Breastplate of Testing";
     private int strength = 0;
     private int agility = 2;
     private int intelligence = 5;
     private int stamina = 3;
-    private String type = "chest";
     
     public String getName(){return this.name;}
     public int[] getAttributes() {
     	return new int[]{this.strength, this.agility, this.intelligence, this.stamina};
-    }
-    public String getType() {
-    	return this.type;
     }
 }
