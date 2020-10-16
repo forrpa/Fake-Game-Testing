eb7c16643708b22d7d05e579242f6dfdb214f883
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 12:33:03 +0200

    refactoring; isChest -> getType

--- src/test/java/equipment/BreastplateOfTestingTest.java
+++ src/test/java/equipment/BreastplateOfTestingTest.java
@@ -18,6 +18,6 @@ public class BreastplateOfTestingTest {
     @Test
     void isChestTest() {
     	BreastplateOfTesting bpt = new BreastplateOfTesting();
-    	assertTrue(bpt.isChest());
+    	assertTrue(bpt.getType().equals("chest"));
     }
 }
