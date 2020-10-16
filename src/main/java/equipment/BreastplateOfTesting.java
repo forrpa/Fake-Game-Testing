f51537e6a9ca94f1caee416221b9f0a7d12a645d
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 13:46:14 +0200

    method created for getting armor value, very basic.

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -5,5 +5,7 @@ public class BreastplateOfTesting extends Chest{
     public BreastplateOfTesting() {
 		super("Breastplate of Testing", "An excellent breastplate for testing things with!", 0, 2, 5, 3);
 	}
-    
+    public int getArmor() {
+    	return 150;
+    }
 }
