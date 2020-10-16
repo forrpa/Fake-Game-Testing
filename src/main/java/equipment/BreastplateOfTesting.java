b68a4b6a4371c4b960120bfdae6bf06e047ff628
chne0432 <chne0432@git.dsv.su.se>
2020-10-16 12:33:03 +0200

    refactoring; isChest -> getType

--- src/main/java/equipment/BreastplateOfTesting.java
+++ src/main/java/equipment/BreastplateOfTesting.java
@@ -6,12 +6,13 @@ public class BreastplateOfTesting{
     private int agility = 2;
     private int intelligence = 5;
     private int stamina = 3;
+    private String type = "chest";
     
     public String getName(){return this.name;}
     public int[] getAttributes() {
     	return new int[]{this.strength, this.agility, this.intelligence, this.stamina};
     }
-    public boolean isChest() {
-    	return true;
+    public String getType() {
+    	return this.type;
     }
 }
