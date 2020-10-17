c2e6f8e82a3429459ce339186c5d60174ae0f188
chne0432 <chne0432@git.dsv.su.se>
2020-10-17 16:57:22 +0200

    Connecting Player and ArmorType

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,13 +1,14 @@
 package player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import equipment.ArmorType;
 
 public class Player {
     private int healthPoint;
-    private int experiencePoint;
-
-
-
+    private int experiencePoint; 
+    private Map<ArmorType, Boolean> allowedArmorTypes = new HashMap<ArmorType, Boolean>();
 
     //private PlayerClass playerClass;
     //private Race race; //Beror på om vi vill göra klasserna Class och Race, jag kör med String sålänge
@@ -21,6 +22,7 @@ public class Player {
         this.race = race;
         this.healthPoint = healthPoint;
         this.experiencePoint = experiencePoint;
+        setArmorTypeHashMap();
     }
 
 
@@ -53,7 +55,23 @@ public class Player {
     public void setExperiencePoint(int experiencePoint) {
         this.experiencePoint = experiencePoint;
     }
-
+    private void setArmorTypeHashMap() {
+    	switch(this.playerClass) {
+    	case "Mage":
+    		this.allowedArmorTypes.put(ArmorType.CLOTH, true);
+    		this.allowedArmorTypes.put(ArmorType.LEATHER, false);
+    		this.allowedArmorTypes.put(ArmorType.MAIL, false);
+    		this.allowedArmorTypes.put(ArmorType.PLATE, false);
+    		break;
+    	case "Warrior":
+    		this.allowedArmorTypes.put(ArmorType.CLOTH, true);
+    		this.allowedArmorTypes.put(ArmorType.LEATHER, true);
+    		this.allowedArmorTypes.put(ArmorType.MAIL, true);
+    		this.allowedArmorTypes.put(ArmorType.PLATE, true);
+    		break;
+    	}
+    	//this.allowedArmorTypes.put("cloth", value)
+    }
 
     // methods added by Christoffer
 }
