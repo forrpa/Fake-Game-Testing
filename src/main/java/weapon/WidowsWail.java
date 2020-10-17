b9130aa3f2f39f55145c50eb9b5007cd1ae72726
chne0432 <chne0432@git.dsv.su.se>
2020-10-17 15:31:59 +0200

    added necessary constructor information for Widow's Wail

--- src/main/java/weapon/WidowsWail.java
+++ src/main/java/weapon/WidowsWail.java
@@ -4,7 +4,7 @@ public class WidowsWail extends Sword{
 	private OneOrTwoHanded ONEHANDED;
 	
 	public WidowsWail() {
-		
+		super("Widow's Wail", "A valyrian steel blade that once belonged to a little shit called Joffrey Baratheon.", new int[]{78, 99}, 102, 17, 14, 0, 10);
 	}
 
 }
