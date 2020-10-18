8c9215463bb85458c61361570bba60aed39b1dae
adamblo <38425775+adamblo@users.noreply.github.com>
2020-10-18 12:02:12 +0200

    Attack class that stores attack info

---
+++ src/main/java/unit/Attack.java
@@ -0,0 +1,13 @@
+package unit;
+
+public class Attack {
+    int attackPower;
+
+    Attack(int attackPower){
+        this.attackPower = attackPower;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+}
