b9cd709b5a3551e32e652e423a8c535ef78906af
chne0432 <chne0432@git.dsv.su.se>
2020-10-20 16:26:11 +0200

    Added a two-handed sword with superclass.

---
+++ src/main/java/weapon/Heartsbane.java
@@ -0,0 +1,9 @@
+package weapon;
+
+public class Heartsbane extends TwoHandedSword{
+
+	public Heartsbane() {
+		super("Heartsbane", "A two-handed greatsword that belonged to a giant twat called Randyll Tarly.", 35, new int[] {160,197}, 117, 46, 28, 0, 18);
+	}
+
+}
---
+++ src/main/java/weapon/TwoHandedSword.java
@@ -0,0 +1,9 @@
+package weapon;
+
+public class TwoHandedSword extends Weapon{
+
+	public TwoHandedSword(String name, String desc, int recLevel, int[] dmg, int dur, int str, int agi, int intell, int sta) {
+		super(name, desc, recLevel, dmg, dur, str, agi, intell, sta);
+	}
+
+}
