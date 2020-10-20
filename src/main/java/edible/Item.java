1c7f1fe2d8841176016163e235b22d0b26ac61ad
mushu <mushu@CP3>
2020-10-20 10:50:29 +0200

    final på item attribut, bort med setters

--- src/main/java/edible/Item.java
+++ src/main/java/edible/Item.java
@@ -2,28 +2,20 @@ package edible;
 
 public abstract class Item {
 
-	private String name;
-	private String description;
+	private final String name;
+	private final String description;
 	
 	public Item(String name, String description ) {
-		this.setName(name);
-		this.setDescription(description);
+		this.name = name;
+		this.description = description;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getDescription() {
 		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
     @Override
