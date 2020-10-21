15d9a2378a5ec29f9b2eb91726a5ebda0f1d1cda
chne0432 <chne0432@git.dsv.su.se>
2020-10-21 14:14:47 +0200

    changed damage in weapons from array to int

--- src/main/java/weapon/TwoHandedSword.java
+++ src/main/java/weapon/TwoHandedSword.java
@@ -3,7 +3,7 @@ package weapon;
 public class TwoHandedSword extends Weapon{
 	private WeaponSize weaponSize = WeaponSize.TwoHanded;
 	
-	public TwoHandedSword(String name, String desc, int recLevel, int[] dmg, int dur, int str, int agi, int intell, int sta) {
+	public TwoHandedSword(String name, String desc, int recLevel, int dmg, int dur, int str, int agi, int intell, int sta) {
 		super(name, desc, recLevel, dmg, dur, str, agi, intell, sta);
 	}
 	@Override
--- src/main/java/weapon/WidowsWail.java
+++ src/main/java/weapon/WidowsWail.java
@@ -3,6 +3,6 @@ package weapon;
 public class WidowsWail extends OneHandedSword{
 	
 	public WidowsWail() {
-		super("Widow's Wail", "A valyrian steel blade that once belonged to a little shit called Joffrey Baratheon.", 20, new int[]{78, 99}, 102, 17, 14, 0, 10);
+		super("Widow's Wail", "A valyrian steel blade that once belonged to a little shit called Joffrey Baratheon.", 20, 87, 102, 17, 14, 0, 10);
 	}
 }
