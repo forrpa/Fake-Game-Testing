6849d58edacdf6a2f8e00926bf178a1f03428a36
adamblo <38425775+adamblo@users.noreply.github.com>
2020-10-26 11:05:40 +0100

    Villager constuctor is public

--- src/main/java/unit/Villager.java
+++ src/main/java/unit/Villager.java
@@ -2,7 +2,7 @@ package unit;
 
 public class Villager extends NPC implements Talkable{
 
-    Villager(String name, int maxHealth, int attackPower, boolean isGrounded, AttackType resistance, AttackType weakness){
+    public Villager(String name, int maxHealth, int attackPower, boolean isGrounded, AttackType resistance, AttackType weakness){
         super(name, maxHealth, attackPower, 50, true, null, null);
     }
 
