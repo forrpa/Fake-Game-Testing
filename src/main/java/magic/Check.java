e86951ea75c4871bec751b26859dc13f174e7560
Christoffer <ccohman@gmail.com>
2020-10-26 18:07:03 +0100

    Added tests to LifeStealSpellTest and refactored some methods in MagicPlayer

--- src/main/java/magic/Check.java
+++ src/main/java/magic/Check.java
@@ -2,18 +2,20 @@
 package magic;
 
 public class Check {
-    // Assure that a number is: >=0
+    // Assure that a number is: >=0 <= Integer.max
     protected static int numberCheck(int nr) {
-        if (nr >= 0) {
-            return nr;
-        } else {
+        if (nr < 0) {
             throw new IllegalArgumentException ("Error: negative numbers are not allowed here");
         }
+        if (nr <Integer.MAX_VALUE) {
+            return (nr);
+        }
+        throw new IllegalThreadStateException ("Error: Buffer Overflow, the number is too big.");
     }
 
     // If i decide to not allow empty strings. add. else remove.
     // Check if Strings are not blank, empty or null.
-    private static void stringCheck(String string) {
+    public static void stringCheck(String string) {
         if (string == null || string.trim ().isEmpty ()) {
             throw new IllegalArgumentException ("Error: a blank string is not allowed here");
         }
