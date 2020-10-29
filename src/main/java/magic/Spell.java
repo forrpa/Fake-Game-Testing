175a8fdba363ba4a88107e552742c8ff3122b07d
Christoffer <ccohman@gmail.com>
2020-10-29 12:52:02 +0100

    removed todo

--- src/main/java/magic/Spell.java
+++ src/main/java/magic/Spell.java
@@ -17,7 +17,6 @@ class Spell {
     private int cooldownDuration = 0;
     // implement isOnCooldown in castSpell.
 
-    // TODO: 2020-10-20 implement "damage interface"
 
     public Spell(String name, String description, int manaCost, int requiredMagicSkill, int cooldownDuration) {
         this.name = stringCheck (name);
