4134b8c7dad833c66748ebe2e2d8cfcdcbe576dc
Jennifer <jemc7787@student.su.se>
2020-10-30 13:07:39 +0100

    Forgot toString, fixed

--- src/test/java/quest/StealthAndAttackEnemyTest.java
+++ src/test/java/quest/StealthAndAttackEnemyTest.java
@@ -10,7 +10,7 @@ class StealthAndAttackEnemyTest {
 
     @Test
     void toStringReturnsCorrectString(){
-        assertEquals("Name: Stealth and attack enemy, Max Health: 8, Current Health: 8, Attack Power: 3, Resistance: null, Weakness null, Coordinates: 254,566", enemy.toString());
+        assertEquals("Name: Stealth and attack enemy, Max Health: 50, Current Health: 50, Attack Power: 3, Resistance: null, Weakness null, Coordinates: 254,566", enemy.toString());
     }
 
 }
